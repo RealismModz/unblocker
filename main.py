ecb198b7076ab9378a432a9e5318ab8c2f68e09f
RealizmModz <74314445+RealismModz@users.noreply.github.com>
2022-03-01 04:45:27 +0000

    e

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import webbot
-import os
 import time
-import random
-driver = webbot.Browser()
-print("penis")
-driver.go_to('discord.com')
+driver = webbot.Browser();
+yt_id = 'UCvgvktJEZQhPZMJWzF4tojg'
+driver.go_to('https://mytoolstown.com/youtube/earn/');
+driver.scrolly(400);
+driver.type(yt_id, into='Channel Link / ID.');
+driver.click('SEARCH CHANNEL');
+driver.go_to('https://mytoolstown.com/youtube/earn/');
+driver.scrolly(300);
+driver.click(id='checkbox1');
+driver.click(text='Like')
 while True:
-	time.sleep(0)
+	time.sleep(0.001)
